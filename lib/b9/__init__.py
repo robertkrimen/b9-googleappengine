cb14a5fb772db161c571e3b663070c2627d1c04b
robertkrimen <robertkrimen@gmail.com>
2008-10-01 16:22:54 -0700

    Added pluralize Added minute_second_millisecond

--- lib/b9/__init__.py
+++ lib/b9/__init__.py
@@ -12,3 +12,20 @@ def empty(value):
 def default(value, default=''):
     return value if not empty(value) else default
 
+def pluralize(count, singular, plural):
+    if count == 1:
+        return singular
+    else:
+        return plural
+
+def minute_second_millisecond(millisecond_time):
+    millisecond = int(millisecond_time % 1000)
+    second = int(millisecond_time / 1000) % 60
+    minute = int(millisecond_time / (60 * 1000))
+    clock = "%02d:%02d.%03d" % (minute, second, millisecond)
+    return {
+        "clock": clock,
+        "millisecond": millisecond,
+        "second": second,
+        "minute": minute,
+    }
