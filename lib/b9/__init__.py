30bdbf4bb470c68877f25f8c877faaaf4e539b32
robertkrimen <robertkrimen@gmail.com>
2008-10-04 12:14:20 -0700

    Cleaned up some parenthesizing

--- lib/b9/__init__.py
+++ lib/b9/__init__.py
@@ -1,7 +1,7 @@
 def extract(dict_, *names):
     result = None
     for name in names:
-        value = dict_.pop(name, None)
+        value = dict_.pop( name, None )
         if result is None and value is not None:
             result = value
     return result
@@ -10,7 +10,7 @@ def empty(value):
     return not(bool(value) or value is 0)
 
 def default(value, default=''):
-    return value if not empty(value) else default
+    return value if not empty( value ) else default
 
 def pluralize(count, singular, plural):
     if count == 1:
@@ -19,10 +19,10 @@ def pluralize(count, singular, plural):
         return plural
 
 def minute_second_millisecond(millisecond_time):
-    millisecond = int(millisecond_time % 1000)
-    second = int(millisecond_time / 1000) % 60
-    minute = int(millisecond_time / (60 * 1000))
-    clock = "%02d:%02d.%03d" % (minute, second, millisecond)
+    millisecond = int( millisecond_time % 1000 )
+    second = int( millisecond_time / 1000 ) % 60
+    minute = int( millisecond_time / (60 * 1000) )
+    clock = "%02d:%02d.%03d" % ( minute, second, millisecond )
     return {
         "clock": clock,
         "millisecond": millisecond,
